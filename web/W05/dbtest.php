79e9add8f32069b0e626ed970f8b15c1216754eb
NathanPrestwich <sirnapsalot@gmail.com>
2020-10-12 11:44:31 -0600

    added a db test php

---
+++ web/W05/dbtest.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DB Test</title>
+</head>
+<body>
+    <?php 
+        try
+        {
+          $dbUrl = getenv('DATABASE_URL');
+        
+          $dbOpts = parse_url($dbUrl);
+        
+          $dbHost = $dbOpts["host"];
+          $dbPort = $dbOpts["port"];
+          $dbUser = $dbOpts["user"];
+          $dbPassword = $dbOpts["pass"];
+          $dbName = ltrim($dbOpts["path"],'/');
+        
+          $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+          echo 'Error!: ' . $ex->getMessage();
+          die();
+        }
+
+        foreach ($db->query('SELECT PlayerId ,Playername FROM Player') as $row)
+        {
+        echo 'Player: ' . $row['PlayerId'];
+        echo ' Player Name: ' . $row['Playername'];
+        echo '<br/>';
+        }
+    ?>
+</body>
+</html>
